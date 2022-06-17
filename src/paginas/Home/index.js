6bd405f0ecea1e5fd785c7813e0a9880c9c6e075
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-17 19:23:14 -0300

    fix: correcao de warnings da home

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Header from '../Main/Header';
 import Footer from '../Main/Footer';
@@ -11,15 +9,7 @@ import Image from 'material-ui-image';
 import ImagemHome from '../Imagens/pexels-photo-5668481.jpg';
 
 
-const useStyles = makeStyles((theme) => ({
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
-}));
-
 export default function Blog() {
-  const classes = useStyles();
-
   return (
     <React.Fragment>
       <CssBaseline />
