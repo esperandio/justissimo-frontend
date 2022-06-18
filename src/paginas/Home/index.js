07a17a9d544d1f773a7d9d3e38a346eeaa1e9f65
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-18 16:18:48 -0300

    fix: ajustes na home

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -2,45 +2,49 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Header from '../Main/Header';
+import { makeStyles } from '@material-ui/core/styles';
+import { TitleJustissimo }  from '../../components/Utils/title';
 import Footer from '../Main/Footer';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Image from 'material-ui-image';
-import ImagemHome from '../Imagens/pexels-photo-5668481.jpg';
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    banner: {
+        width: '20vw',
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
-export default function Blog() {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <Header title="Justíssimo"/>
-      </Container>
-      <Box sx={{mx: 'auto', width: {
-        xs: 300,
-        sm: 500,
-        md: 700,
-        lg: 1000,
-        xl: 2000},
-      }}>
-        <Typography variant="h2" component="h2" color="inherit" align="center" noWrap>
-          Justíssimo
-        </Typography>
-        <Typography variant="body1" gutterBottom align="justify">
-          Justíssimo trata-se de uma fundamental ferramenta para democratizar o conhecimento jurídico, e de forma gratuita. 
-          Além disso, auxilia muitos advogados em início de carreira, pelos serviços de correspondência.
-          Tem, ainda os clientes procuram os advogados fácil.
-        </Typography>
-        <Typography variant="body1" gutterBottom align="justify">
-          Somos uma starput que une Direito e Tecnologia para fazer com que a justiça dos tribunais e chegue às casas de 
-          qualquer cidadão ou cidadã, suas decisões e por meio da informação.
-        <Image
-          src={ImagemHome}
-        />
-        </Typography>
-      </Box>
+export default function Home() {
+    const classes = useStyles();
 
-      <Footer />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <Header title="Home" />
+
+                <div className={classes.paper}>
+                    <TitleJustissimo/>
+                </div>
+
+                <Typography variant="body1" gutterBottom align="justify">
+                    Justíssimo trata-se de uma fundamental ferramenta para democratizar o conhecimento jurídico, e de forma gratuita. 
+                    Além disso, auxilia muitos advogados em início de carreira, pelos serviços de correspondência.
+                    Tem, ainda os clientes procuram os advogados fácil.
+                </Typography>
+
+                <Typography variant="body1" gutterBottom align="justify">
+                    Somos uma starput que une Direito e Tecnologia para fazer com que a justiça dos tribunais e chegue às casas de qualquer cidadão ou cidadã, suas decisões e por meio da informação.
+                </Typography>
+            </Container>
+            <Footer />
+        </React.Fragment>
+    );
 }
