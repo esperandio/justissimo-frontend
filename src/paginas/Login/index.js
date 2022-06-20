a8ea8e89903ce9eec70e3672cd0a4bffd7c45e85
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-20 20:45:05 -0300

    fix: remocao do campo para manter conectado no login, visto que nao esta implementado

--- src/paginas/Login/index.js
+++ src/paginas/Login/index.js
@@ -4,8 +4,6 @@ import api from '../../service/api';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
@@ -134,12 +132,6 @@ export default function Login() {
                         autoComplete="current-senha"
                         value={senha}
                         onChange={e => setSenha(e.target.value)}
-                    />
-
-                    {/* Checkbox 'Manter conectado' */}
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Manter conectado"
                     />
 
                     {/* Button 'Login' */}
