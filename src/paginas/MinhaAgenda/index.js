c1bab14a73c49ad4c967aa2bc6d03e10656533db
gabriel_ratke <gabrielratke1@gmail.com>
2022-06-21 20:35:08 -0300

    comit

--- src/paginas/MinhaAgenda/index.js
+++ src/paginas/MinhaAgenda/index.js
@@ -93,50 +93,54 @@ export default function MinhaAgenda() {
       
   return (
     <React.Fragment>
-            <CssBaseline />
-            <Container maxWidth="lg">
-                <div>
-                  <TitleJustissimo/>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <div>
+          <TitleJustissimo/>
 
-                  <h2>Minha Agenda</h2>
+          <h2>Minha Agenda</h2>
 
-                  {agendas.map((agenda) => {
-                    return <>
-                      <Card id="myTable" sx={{ maxWidth: 500 }}>
-                        <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
-                          Matheus Esperandio
-                          </Typography>
-                          
-                          <Typography gutterBottom variant="h5" component="div">
-                          Causa: Trabalhista
-                          </Typography>
+          {agendas.map((agenda) => {
+            return (
+              <Card key={agenda.id_agenda} id="myTable" sx={{ maxWidth: 500 }}>
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">
+                  {agenda.cliente.nome}
+                  {' | '}
+                  Causa: Trabalhista
+                  </Typography>
 
-                          <Typography gutterBottom variant="h6" component="div">
-                          08/04/2022
-                          </Typography>
+                  
+                  
+                  <Typography gutterBottom variant="h6" component="div">
+                  Causa: Trabalhista
+                  </Typography>
 
-                          <Typography gutterBottom variant="h7" component="div">
-                          Sexta-Feira
-                          </Typography>
+                  <Typography gutterBottom variant="h7" component="div">
+                  {agenda.data_agendamento}
+                  </Typography>
 
-                          <Typography gutterBottom variant="h8" component="div">
-                          15:00h
-                          </Typography>
+                  <Typography gutterBottom variant="h8" component="div">
+                  Sexta-Feira
+                  </Typography>
 
-                          <Typography gutterBottom variant="h6" component="div">
-                          Contato em {agenda.contato_cliente}
-                          </Typography>
-                        </CardContent>
-                        <CardActions>
-                              <Button size="small" color="primary">
-                                  ENCERRAR
-                              </Button>
-                        </CardActions>
-                      </Card>
-                    </>
-                  })}
-                </div>
+                  <Typography gutterBottom variant="h8" component="div">
+                  15:00h{agenda.horario}
+                  </Typography>
+
+                  <Typography gutterBottom variant="h7" component="div">
+                  Contato em {agenda.contato_cliente}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                      <Button>
+                          ENCERRAR
+                      </Button>
+                </CardActions>
+              </Card>
+            )
+          })}
+        </div>
       </Container>
     </React.Fragment>
                     
