27154366cc54634a27341582157b8b719a9aa8ff
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-21 23:41:14 -0300

    fix: remocao de propriedade invalida

--- src/paginas/CasosDeUso/Avaliacao/Advogado/index.js
+++ src/paginas/CasosDeUso/Avaliacao/Advogado/index.js
@@ -151,7 +151,6 @@ export default function AvaliacaoAdvogado() {
 
                         <div 
                             style={{
-                                "font-family": "'Inter', sans-serif",
                                 "color": '#3B485E'
                             }}
                         >
