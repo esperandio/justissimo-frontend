884207174c552a5d72685caf3fd7ad171c9e819b
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-06-22 21:26:12 -0300

    fix: remocao da propriedade defaultValue do componente de InputCEPMask (estava gerando erro no console)

--- src/components/Utils/mask/inputCepMask.js
+++ src/components/Utils/mask/inputCepMask.js
@@ -3,7 +3,7 @@ import React from "react";
 import InputMask from "react-input-mask";
 
 const InputCEPMask = ({value, onChange}) => (
-  <InputMask mask="99999-999" value={value} onChange={onChange} >{() => <TextField  label="CEP" margin="normal" defaultValue="CEP" variant="outlined"/>}</InputMask>
+  <InputMask mask="99999-999" value={value} onChange={onChange} >{() => <TextField  label="CEP" margin="normal" variant="outlined"/>}</InputMask>
 );
 
 export default InputCEPMask;
