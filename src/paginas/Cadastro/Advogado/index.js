07628b0deaf5e72a1492aaf3700af403b48077c2
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-08-29 22:10:04 -0300

    feat: inclusao de todos os campos necessarios para o cadastro de advogado

--- src/paginas/Cadastro/Advogado/index.js
+++ src/paginas/Cadastro/Advogado/index.js
@@ -1,18 +1,262 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Header from '../../Main/Header';
 import Footer from '../../Main/Footer';
 import { TitleJustissimo, TitlePage } from '../../../components/Utils/title';
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { ptBR } from "date-fns/locale";
+import Autocomplete from '@mui/material/Autocomplete';
+import InputCnpjMask from '../../../components/Utils/mask/inputCnpjMask';
+import InputCpfMask from '../../../components/Utils/mask/inputCpfMask';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputCepMask from '../../../components/Utils/mask/inputCepMask';
+import InputTelefone from '../../../components/Utils/mask/inputTelefoneMask';
 
 export default function CadastroAdvogado() {
+    const [nome, setNome] = useState('');
+    const [dt_nascimento, setNascimento] = useState(null);
+    const [tipoPessoa, setTipoPessoa] = useState('Física');
+    const [cpf, setCPF] = useState('');
+    const [cnpj, setCNPJ] = useState('');
+    const [cep, setCEP] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [estado, setEstado] = useState('');
+    const [nr_cna, setNumeroCNA] = useState('');
+    const [estado_cna, setEstadoCNA] = useState('');
+    const [tel_celular, setTelefoneCelular] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [senhaConfirmacao, setSenhaConfirmacao] = useState('');
+
+    const [tiposPessoa] = useState(['Física', 'Jurídica']);
+    const [estados] = useState(['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA',
+    'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']);
+
+    function handleCadastro(e) {
+        e.preventDefault();
+
+        console.log("handleCadastro");
+    }
+
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="lg">
-                <Header title="Cadastrar Cliente" />
+                <Header title="Cadastrar Advogado" />
                 <TitleJustissimo />
-                <TitlePage internal="Cadastro de cliente" />
+                <TitlePage internal="Cadastro de advogado" />
+
+                <form onSubmit={ handleCadastro }>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm container spacing={1}>
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        required
+                                        id="Nome"
+                                        label="Nome"
+                                        placeholder="Digite o nome completo"
+                                        multiline
+                                        variant="outlined"
+                                        value={nome}
+                                        onChange={e => setNome(e.target.value)}
+                                        margin="normal"
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth>
+                                    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+                                        <DatePicker
+                                            label="Data de nascimento"
+                                            value={dt_nascimento}
+                                            onChange={newValue => setNascimento(newValue)}
+                                            renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" />}
+                                        />
+                                    </LocalizationProvider>
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth>
+                                    <Autocomplete
+                                        options={ tiposPessoa }
+                                        isOptionEqualToValue={(option, value) => option.value === value.value}
+                                        onChange={ (_, v) => { setTipoPessoa(v); setCPF(""); setCNPJ("") } }
+                                        clearIcon={ false }
+                                        value={ tipoPessoa }
+                                        renderInput={(params) => <TextField {...params}  variant="outlined" margin="normal" multiline label="Tipo de pessoa" />}
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="Tipo"></InputLabel>
+                                    {tipoPessoa === "Física"
+                                        ? <InputCpfMask required onChange={e => setCPF(e.target.value)} />
+                                        : <InputCnpjMask required onChange={e => setCNPJ(e.target.value)} />
+                                    }
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={4}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="Tipo"></InputLabel>
+                                    <InputCepMask  
+                                        value={cep}
+                                        onChange={e => setCEP(e.target.value)}
+                                        required
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={4}>
+                                <FormControl fullWidth>
+                                    <Autocomplete
+                                        options={estados}
+                                        isOptionEqualToValue={(option, value) => option.value === value.value}
+                                        onChange={ (_, v) => { setEstado(v); } }
+                                        renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" multiline label="Estado" />}
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={4}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        required
+                                        id="Cidade"
+                                        label="Cidade"
+                                        placeholder="Nome da Cidade"
+                                        multiline
+                                        variant="outlined"
+                                        value={cidade}
+                                        onChange={e => setCidade(e.target.value)}
+                                        margin="normal"
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={4}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        required
+                                        id="nr_cna"
+                                        label="CNA"
+                                        placeholder="N° do CNA"
+                                        variant="outlined"
+                                        value={nr_cna}
+                                        onChange={e => setNumeroCNA(e.target.value)}
+                                        margin="normal"
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={4}>
+                                <FormControl fullWidth>
+                                    <Autocomplete
+                                        options={estados}
+                                        isOptionEqualToValue={(option, value) => option.value === value.value}
+                                        onChange={ (_, v) => { setEstadoCNA(v); } }
+                                        renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" multiline label="Estado do CNA" />}
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={4}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="Telefone"></InputLabel>
+                                    <InputTelefone
+                                        onChange={e => setTelefoneCelular(e.target.value)}
+                                        required
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={12}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        required
+                                        id="email"
+                                        label="E-mail"
+                                        placeholder="meuemail@email.com"
+                                        multiline
+                                        variant="outlined"
+                                        value={email}
+                                        onChange={e => setEmail(e.target.value)}
+                                        margin="normal"
+                                    />
+                                </FormControl>
+                            </Grid>
+                            
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="senha"
+                                        label="Senha"
+                                        type="password"
+                                        id="senha"
+                                        autoComplete="current-senha"
+                                        value={senha}
+                                        onChange={e => setSenha(e.target.value)}
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="senha"
+                                        label="Confirmar senha"
+                                        type="password"
+                                        id="senha"
+                                        autoComplete="current-senha"
+                                        value={senhaConfirmacao}
+                                        onChange={e => setSenhaConfirmacao(e.target.value)}
+                                    />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} sm={12}>
+                                <span>Mínimo 8 caracteres</span> <br/>
+                                <span>Caracteres maiúsculos</span> <br/>
+                                <span>Caracteres mínusculos</span> <br/>
+                                <span>Símbolos ou números</span> <br/>
+                            </Grid>
+
+                            <Grid item xs={12} sm={12}>
+                                <FormControl>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        type="submit"
+                                        startIcon={<SaveIcon />}
+                                    >
+                                        Cadastrar
+                                    </Button>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </form>
                 <Footer />
             </Container>
         </React.Fragment>
