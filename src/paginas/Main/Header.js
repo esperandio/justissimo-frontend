0f69a5abd6b1ec69fac1b46c1c6f7b09b5e02975
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-05 22:49:51 -0300

    feat: inclusao de texto no cabecalho

--- src/paginas/Main/Header.js
+++ src/paginas/Main/Header.js
@@ -89,6 +89,23 @@ const Header = () => {
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <BalanceIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              JUSTÍSSIMO
+            </Typography>
 
             <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
               <IconButton
@@ -132,6 +149,7 @@ const Header = () => {
               variant="h5"
               noWrap
               component="a"
+              href=""
               sx={{
                 mr: 2,
                 display: { xs: 'flex', md: 'none' },
@@ -142,7 +160,9 @@ const Header = () => {
                 color: 'inherit',
                 textDecoration: 'none',
               }}
-            />
+            >
+              JUSTÍSSIMO
+            </Typography>
 
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
               {pages.map((page) => (
