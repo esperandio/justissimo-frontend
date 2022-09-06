ddb6bc534663c4748324ebdad535e63153fdd02f
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-05 23:17:09 -0300

    refactor: renomeacao de diretorios

---
+++ src/pages/EditarPerfil/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Header from '../Main/Header';
+import Footer from '../Main/Footer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export default function EditarPerfil() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Header />
+            <Container maxWidth="lg">
+                Teste
+            </Container>
+            <Footer />
+        </React.Fragment>
+    )
+}
