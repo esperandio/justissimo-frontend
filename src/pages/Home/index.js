f6f9c7dc96a92448d260ac470cea0ec68b7ca90e
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-07 10:58:49 -0300

    wip: revisao do layout da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,81 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
 import Header from '../Main/Header';
-import { TitleJustissimo }  from '../../components/Utils/title';
 import Footer from '../Main/Footer';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import WatsonComponent from '../../components/watson/watson_component';
 
 export default function Home() {
     return (
         <React.Fragment>
-            <WatsonComponent/>
+            <WatsonComponent />
             <CssBaseline />
             <Header />
-            <Container maxWidth="lg">
-                <TitleJustissimo/>
+            <Container>
+                <Stack
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                    sx={{ height: '60vh' }}
+                >
+                    <Typography variant="h2" gutterBottom align='center' style={{ fontWeight: 600 }}>
+                        The Progressive <br/>JavaScript Framework
+                    </Typography>
 
-                <Typography variant="body1" gutterBottom align="justify">
-                    Justíssimo trata-se de uma fundamental ferramenta para democratizar o conhecimento jurídico, e de forma gratuita. 
-                    Além disso, auxilia muitos advogados em início de carreira, pelos serviços de correspondência.
-                    Tem, ainda os clientes procuram os advogados fácil.
-                </Typography>
+                    <Typography variant="subtitle1" gutterBottom align='center' style={{ fontStyle: 'italic' }}>
+                        An approachable, performant and versatile framework for building web user interfaces. 
+                    </Typography>
+                </Stack>
 
-                <Typography variant="body1" gutterBottom align="justify">
-                    Somos uma starput que une Direito e Tecnologia para fazer com que a justiça dos tribunais e chegue às casas de qualquer cidadão ou cidadã, suas decisões e por meio da informação.
-                </Typography>
+                <Stack
+                    direction={{ xs: "column", md: "row" }} 
+                    justifyContent="center"
+                    spacing={2}
+                >
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        spacing={2}
+                    >
+                        <Typography variant="h6" style={{ fontWeight: 600 }}>
+                            Approachable
+                        </Typography>
+
+                        <Typography variant="subtitle1" gutterBottom>
+                            Builds on top of standard HTML, CSS and JavaScript with intuitive API and world-class documentation. 
+                        </Typography>
+                    </Stack>
+
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        spacing={2}
+                    >
+                        <Typography variant="h6" style={{ fontWeight: 600 }}>
+                            Performant
+                        </Typography>
+
+                        <Typography variant="subtitle1" gutterBottom>
+                            Truly reactive, compiler-optimized rendering system that rarely requires manual optimization. 
+                        </Typography>
+                    </Stack>
+
+                    <Stack
+                        direction="column"
+                        justifyContent="center"
+                        spacing={2}
+                    >
+                        <Typography variant="h6" style={{ fontWeight: 600 }}>
+                            Versatile
+                        </Typography>
+
+                        <Typography variant="subtitle1" gutterBottom>
+                            A rich, incrementally adoptable ecosystem that scales between a library and a full-featured framework. 
+                        </Typography>
+                    </Stack>
+                </Stack>
             </Container>
             <Footer />
         </React.Fragment>  
