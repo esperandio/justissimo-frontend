e195c01e93217a2c2b3a499440140ac2833fb06d
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-10 14:18:20 -0300

    wip: inclusao dos campos disponiveis para alteracao na edicao do perfil

--- src/pages/EditarPerfil/index.js
+++ src/pages/EditarPerfil/index.js
@@ -12,12 +12,29 @@ import FileUploadIcon from '@mui/icons-material/FileUpload';
 import SaveIcon from '@material-ui/icons/Save';
 import UserDefaultIcon from '../../user.svg';
 import Stack from '@mui/material/Stack';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { ptBR } from "date-fns/locale";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputCpfMask from '../../components/Utils/mask/inputCpfMask';
+import InputCnpjMask from '../../components/Utils/mask/inputCnpjMask';
+import InputCepMask from '../../components/Utils/mask/inputCepMask';
+import Autocomplete from '@mui/material/Autocomplete';
 
 export default function EditarPerfil() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
-  const [info, setInfo] = useState('');
+  const [dt_nascimento, setNascimento] = useState(null);
+  const [cpf, setCPF] = useState('');
+  const [cnpj, setCNPJ] = useState('');
+  const [cep, setCEP] = useState('');
+  const [estado, setEstado] = useState('');
+  const [cidade, setCidade] = useState('');
   const [imagemPreview, setImagemPreview] = useState(null);
+
+  const [estados] = useState(['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA',
+  'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']);
 
   const onImageChange = (event) => {
     const file = event.target.files[0];
@@ -45,7 +62,7 @@ export default function EditarPerfil() {
                   <img 
                     src={imagemPreview == null ? UserDefaultIcon : imagemPreview} 
                     alt="preview" 
-                    width={'200px'}
+                    width={'180px'}
                   />
 
                   <FormControl>
@@ -71,30 +88,15 @@ export default function EditarPerfil() {
                 </Stack>
               </Grid>
 
-              {/* E-mail */}
+              {/* Home */}
               <Grid item xs={12} sm={6}>
                 <FormControl fullWidth>
                   <TextField
                     required
-                    id="email"
-                    label="E-mail"
-                    placeholder="meuemail@email.com"
-                    variant="outlined"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    margin="normal"
-                  />
-                </FormControl>
-              </Grid>
-
-              {/* Nome */}
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <TextField 
-                    required
                     id="Nome"
                     label="Nome"
                     placeholder="Digite o nome completo"
+                    multiline
                     variant="outlined"
                     value={nome}
                     onChange={e => setNome(e.target.value)}
@@ -103,16 +105,90 @@ export default function EditarPerfil() {
                 </FormControl>
               </Grid>
 
-              {/* Descrição */}
+              {/* Data de nascimento */}
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth>
+                  <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+                    <DatePicker
+                      label="Data de nascimento"
+                      value={dt_nascimento}
+                      onChange={newValue => setNascimento(newValue)}
+                      renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" />}
+                    />
+                  </LocalizationProvider>
+                </FormControl>
+              </Grid>
+
+              {/* CPF */}
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth>
+                  <InputLabel id="Tipo"></InputLabel>
+                  <InputCpfMask value={cpf} required onChange={e => setCPF(e.target.value)} />
+                </FormControl>
+              </Grid>
+
+              {/* CNPJ */}
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth>
+                  <InputLabel id="Tipo"></InputLabel>
+                  <InputCnpjMask value={cnpj} required onChange={e => setCNPJ(e.target.value)} />
+                </FormControl>
+              </Grid>
+
+              {/* CEP */}
+              <Grid item xs={12} sm={4}>
+                <FormControl fullWidth>
+                  <InputLabel id="Tipo"></InputLabel>
+                  <InputCepMask  
+                    value={cep}
+                    onChange={e => setCEP(e.target.value)}
+                    required
+                  />
+                </FormControl>
+              </Grid>
+
+              {/* Estado */}
+              <Grid item xs={12} sm={4}>
+                <FormControl fullWidth>
+                  <Autocomplete
+                    options={estados}
+                    value={estado}
+                    isOptionEqualToValue={(option, value) => option.value === value.value}
+                    onChange={ (_, v) => { setEstado(v); } }
+                    renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" label="Estado" />}
+                  />
+                </FormControl>
+              </Grid>
+
+              {/* Cidade */}
+              <Grid item xs={12} sm={4}>
+                <FormControl fullWidth>
+                  <TextField
+                    required
+                    id="Cidade"
+                    label="Cidade"
+                    placeholder="Nome da Cidade"
+                    multiline
+                    variant="outlined"
+                    value={cidade}
+                    onChange={e => setCidade(e.target.value)}
+                    margin="normal"
+                  />
+                </FormControl>
+              </Grid>
+
+              {/* E-mail */}
               <Grid item xs={12} sm={12}>
                 <FormControl fullWidth>
                   <TextField
                     required
+                    id="email"
+                    label="E-mail"
+                    placeholder="meuemail@email.com"
                     multiline
-                    label="Descrição do perfil"
                     variant="outlined"
-                    value={info}
-                    onChange={e => setInfo(e.target.value)}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                     margin="normal"
                   />
                 </FormControl>
