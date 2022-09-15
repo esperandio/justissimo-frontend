25f6e4a38c03a8df628939f5be346e96a354be8a
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-14 23:34:09 -0300

    style: ajuste em espacamento

--- src/pages/Listar/Advogado/index.js
+++ src/pages/Listar/Advogado/index.js
@@ -20,208 +20,211 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-    },
-    user: {
-        width: '20vh',
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    }
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  user: {
+    width: '20vh',
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  }
 }));
 
 export default function ListarAdvogado() {
-    const classes = useStyles();
-    const history = useHistory();
+  const classes = useStyles();
+  const history = useHistory();
 
-    const [nome, setNome] = useState("");
-    const [advogados, setAdvogados] = useState([]);
-    const [usarFiltroAvancado, setUsarFiltroAvancado] = useState(false);
-    const [id_area_atuacao, setAreaAtuacao] = useState("");
-    const [estado, setEstado] = useState("");
-    const [cidade, setCidade] = useState("");
+  const [nome, setNome] = useState("");
+  const [advogados, setAdvogados] = useState([]);
+  const [usarFiltroAvancado, setUsarFiltroAvancado] = useState(false);
+  const [id_area_atuacao, setAreaAtuacao] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cidade, setCidade] = useState("");
 
-    const [areas, setAreas] = useState([]);
-    const [estados] = useState(['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA',
-    'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']);
+  const [areas, setAreas] = useState([]);
+  const [estados] = useState(['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA',
+  'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']);
 
-    useEffect(() => {
-        async function buscarAdvogados() {
-            const resultado = await api.get(`lawyers`);
-            setAdvogados(resultado.data);
-        }
-
-        async function buscarAreas() {
-            const resultado = await api.get('areas');
-            setAreas(resultado.data);
-        }
-
-        buscarAdvogados();
-        buscarAreas();
-    }, []);
-
-    function handleAutocompleteAreaChange(event, values) {
-        setAreaAtuacao(values?.id);
+  useEffect(() => {
+    async function buscarAdvogados() {
+      const resultado = await api.get(`lawyers`);
+      setAdvogados(resultado.data);
     }
 
-    function handleAutocompleteEstadoChange(event, values) {
-        setEstado(values);
+    async function buscarAreas() {
+      const resultado = await api.get('areas');
+      setAreas(resultado.data);
     }
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+    buscarAdvogados();
+    buscarAreas();
+  }, []);
 
-        let filtroApi = `nome=${nome}`;
+  function handleAutocompleteAreaChange(event, values) {
+    setAreaAtuacao(values?.id);
+  }
 
-        if (usarFiltroAvancado === true) {
-            filtroApi = `${filtroApi}&area=${id_area_atuacao ?? ""}&estado=${estado ?? ""}&cidade=${cidade ?? ""}`;
-        }
+  function handleAutocompleteEstadoChange(event, values) {
+    setEstado(values);
+  }
 
-        try {
-            const resultado = await api.get(`lawyers?${filtroApi}`);
-            setAdvogados(resultado.data);
-        } catch (error) {
-            const mensagem_retorno_api = error?.response?.data?.message;
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-            if (mensagem_retorno_api == null) {
-                alert(`🤨 Algo deu errado! Tente novamente mais tarde`);
-                return ;
-            }
+    let filtroApi = `nome=${nome}`;
 
-            alert(mensagem_retorno_api);
-        }
+    if (usarFiltroAvancado === true) {
+      filtroApi = `${filtroApi}&area=${id_area_atuacao ?? ""}&estado=${estado ?? ""}&cidade=${cidade ?? ""}`;
     }
 
-    async function handleExibirAdvogado(id_advogado) {
-        history.push(`/advogado/${id_advogado}`);
+    try {
+      const resultado = await api.get(`lawyers?${filtroApi}`);
+      setAdvogados(resultado.data);
+    } catch (error) {
+      const mensagem_retorno_api = error?.response?.data?.message;
+
+      if (mensagem_retorno_api == null) {
+        alert(`🤨 Algo deu errado! Tente novamente mais tarde`);
+        return ;
+      }
+
+      alert(mensagem_retorno_api);
     }
+  }
 
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <Header />
-            <Container maxWidth="lg">
-                <TitlePage internal="Busque advogados" />
+  async function handleExibirAdvogado(id_advogado) {
+    history.push(`/advogado/${id_advogado}`);
+  }
 
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm container spacing={1}>
-                        <Grid item xs={12} sm={12}>
-                            <FormControl fullWidth>
-                                <TextField
-                                    id="Pesquisa"
-                                    label="Nome do advogado"
-                                    placeholder="Nome do advogado"
-                                    variant="outlined"
-                                    margin="normal"
-                                    value={nome}
-                                    onChange={e => setNome(e.target.value)}
-                                />
-                            </FormControl>
-                        </Grid>
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Container maxWidth="lg">
+        <TitlePage internal="Busque advogados" />
 
-                        <Grid item xs={12} sm={12}>
-                            <FormControlLabel
-                                control={<Switch checked={usarFiltroAvancado} color="primary" onChange={() => {
-                                    setUsarFiltroAvancado((prev) => !prev);
-                                }} />}
-                                label="Filtros avançados"
-                            />
-                            <Collapse in={usarFiltroAvancado}>
-                                <Grid item xs={12} sm container spacing={1}>
-                                    <Grid item xs={12} sm={4}>
-                                        <FormControl fullWidth variant="outlined" margin="normal">
-                                        <Autocomplete
-                                            options={areas.map((x) => { 
-                                                return {
-                                                    label: x.titulo,
-                                                    id: x.id_area_atuacao
-                                                }
-                                            })}
-                                            renderInput={(params) => <TextField {...params} label="Área de atuação" />}
-                                            isOptionEqualToValue={(option, value) => option.value === value.value}
-                                            onChange={ handleAutocompleteAreaChange }
-                                        />
-                                        </FormControl>
-                                    </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm container spacing={1}>
+            <Grid item xs={12} sm={12}>
+              <FormControl fullWidth>
+                <TextField
+                  id="Pesquisa"
+                  label="Nome do advogado"
+                  placeholder="Nome do advogado"
+                  variant="outlined"
+                  margin="normal"
+                  value={nome}
+                  onChange={e => setNome(e.target.value)}
+                />
+              </FormControl>
+            </Grid>
 
-                                    <Grid item xs={12} sm={4}>
-                                        <FormControl fullWidth variant="outlined" margin="normal">
-                                        <Autocomplete
-                                            options={estados}
-                                            renderInput={(params) => <TextField {...params} label="Estado" />}
-                                            isOptionEqualToValue={(option, value) => option.value === value.value}
-                                            onChange={ handleAutocompleteEstadoChange }
-                                        />
-                                        </FormControl>
-                                    </Grid>
-
-                                    <Grid item xs={12} sm={4}>
-                                        <FormControl fullWidth>
-                                            <TextField
-                                                id="Cidade"
-                                                label="Cidade"
-                                                placeholder="Cidade"
-                                                variant="outlined"
-                                                margin="normal"
-                                                value={cidade}
-                                                onChange={e => setCidade(e.target.value)}
-                                            />
-                                        </FormControl>
-                                    </Grid>
-                                </Grid>
-                            </Collapse>
-                        </Grid>
-
-                        <Grid item xs={12} sm={12}>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                type="submit"
-                                startIcon={<FilterAltIcon />}
-                                onClick={ handleSubmit }
-                            >
-                                Filtrar
-                            </Button>
-                        </Grid>
+            <Grid item xs={12} sm={12}>
+                <FormControlLabel
+                  control={<Switch checked={usarFiltroAvancado} color="primary" onChange={() => {
+                    setUsarFiltroAvancado((prev) => !prev);
+                  }} />}
+                  label="Filtros avançados"
+                />
+                <Collapse in={usarFiltroAvancado}>
+                  <Grid item xs={12} sm container spacing={1}>
+                    <Grid item xs={12} sm={4}>
+                      <FormControl fullWidth variant="outlined" margin="normal">
+                      <Autocomplete
+                        options={areas.map((x) => { 
+                          return {
+                            label: x.titulo,
+                            id: x.id_area_atuacao
+                          }
+                        })}
+                        renderInput={(params) => <TextField {...params} label="Área de atuação" />}
+                        isOptionEqualToValue={(option, value) => option.value === value.value}
+                        onChange={ handleAutocompleteAreaChange }
+                      />
+                      </FormControl>
                     </Grid>
-                </Grid>
 
-                {advogados.length === 0 ? <h3>Nenhum advogado encontrado</h3> : ""}
-
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm container spacing={1}>
-                    {advogados.map((advogado)=>{
-                        return  <Grid key={advogado.id_advogado} item xs={12} sm={4} className={classes.submit}>
-                                    <div className={classes.paper}>
-                                        <img src={logo} className={classes.user} alt="logo" />
-                                        <h2>{advogado.nome}</h2>
-                                        <Rating 
-                                            id="nota"
-                                            name="nota" 
-                                            size='large'
-                                            autoFocus
-                                            readOnly
-                                            value={advogado.nota}
-                                        />
-                                        <p>{advogado._count?.avaliacoes} avaliações</p>
-
-                                        <Button className={classes.submit}
-                                            variant="contained"
-                                            type="submit"
-                                            onClick={ () => handleExibirAdvogado(advogado.id_advogado) }
-                                        >
-                                            Visualizar
-                                        </Button>
-                                    </div>
-                                </Grid>
-                    })}
+                    <Grid item xs={12} sm={4}>
+                      <FormControl fullWidth variant="outlined" margin="normal">
+                      <Autocomplete
+                        options={estados}
+                        renderInput={(params) => <TextField {...params} label="Estado" />}
+                        isOptionEqualToValue={(option, value) => option.value === value.value}
+                        onChange={ handleAutocompleteEstadoChange }
+                      />
+                      </FormControl>
                     </Grid>
-                </Grid>
-            </Container>
-            <Footer />
-        </React.Fragment>
-    );
+
+                    <Grid item xs={12} sm={4}>
+                      <FormControl fullWidth>
+                        <TextField
+                          id="Cidade"
+                          label="Cidade"
+                          placeholder="Cidade"
+                          variant="outlined"
+                          margin="normal"
+                          value={cidade}
+                          onChange={e => setCidade(e.target.value)}
+                        />
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                </Collapse>
+            </Grid>
+
+            <Grid item xs={12} sm={12}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                startIcon={<FilterAltIcon />}
+                onClick={ handleSubmit }
+              >
+                Filtrar
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {advogados.length === 0 ? <h3>Nenhum advogado encontrado</h3> : ""}
+
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm container spacing={1}>
+            {advogados.map((advogado)=>{
+                return (
+                  <Grid key={advogado.id_advogado} item xs={12} sm={4} className={classes.submit}>
+                    <div className={classes.paper}>
+                      <img src={logo} className={classes.user} alt="logo" />
+                      <h2>{advogado.nome}</h2>
+                      <Rating 
+                        id="nota"
+                        name="nota" 
+                        size='large'
+                        autoFocus
+                        readOnly
+                        value={advogado.nota}
+                      />
+
+                      <p>{advogado._count?.avaliacoes} avaliações</p>
+
+                      <Button className={classes.submit}
+                        variant="contained"
+                        type="submit"
+                        onClick={ () => handleExibirAdvogado(advogado.id_advogado) }
+                      >
+                        Visualizar
+                      </Button>
+                    </div>
+                  </Grid>
+                )
+            })}
+          </Grid>
+        </Grid>
+      </Container>
+      <Footer />
+    </>
+  );
 }
