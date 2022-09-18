8ffe08813805e5617dce142a2478e5a7e10d9ac8
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-18 19:52:36 -0300

    feat: criacao de servico para chamar API que realiza o agendamento manual

--- src/services/lawyer.service.js
+++ src/services/lawyer.service.js
@@ -4,6 +4,18 @@ class LawyerService {
   getLawyer(lawyer_id) {
     return api.get(`lawyers/${lawyer_id}`);
   }
+
+  createScheduling(lawyer_id, { fk_advogado_area, nome_cliente, email_cliente, data_agendamento, horario, observacao }) {
+    return api.post(`lawyers/scheduling`, {
+      fk_advogado: lawyer_id,
+      fk_advogado_area, 
+      nome_cliente, 
+      email_cliente, 
+      data_agendamento, 
+      horario, 
+      observacao
+    })
+  }
 }
 
 export default new LawyerService();
