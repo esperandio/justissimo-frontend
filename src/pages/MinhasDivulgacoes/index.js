3c242b40705fed7ef8761d64728e8fae17ad1505
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-29 21:21:00 -0300

    feat: implementacao de chamada ao servico de cliente para recuperar lista de divulgacoes e renderizar na tela

--- src/pages/MinhasDivulgacoes/index.js
+++ src/pages/MinhasDivulgacoes/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Main/Header";
 import { ValidarAutenticacaoCliente } from "../../components/ValidarAutenticacao";
 import { Container, Link, Grid } from "@material-ui/core/";
@@ -9,8 +9,33 @@ import {
   Divider
 } from "@mui/material";
 import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import { ClientService } from "../../services";
 
 export default function MinhasDivulgacoes() {
+  const [divulgacoes, setDivulgacoes] = useState([]); 
+
+  useEffect(() => {
+    async function buscarMinhasDivulgacoes() {
+      const id = parseInt(sessionStorage.getItem("id_cliente"));
+
+      const resultado = await ClientService.getAllDivulgations(id);
+
+      setDivulgacoes(resultado.data);
+    }
+
+    buscarMinhasDivulgacoes();
+  }, []);
+
+  function formatDate(date) {
+    date = new Date(date);
+
+    return `${date.getUTCDate()}`.padStart(2, 0)
+      + "/"
+      + `${date.getUTCMonth() + 1}`.padStart(2, 0)
+      + "/"
+      + `${date.getUTCFullYear()}`;
+  }
+
   return (
     <>
       <ValidarAutenticacaoCliente />
@@ -18,25 +43,47 @@ export default function MinhasDivulgacoes() {
       <Container maxWidth="lg">
         <TitlePage internal="Minhas Divulgações" />
 
-        <Grid container>
-          <Grid item xs={12}>
-            <Chip variant="outlined" color="primary" size="small" icon={<EventAvailableIcon />} label="28/09/2022" />
-            <br />
-            <br />
-            <Link href="/cliente/minhas-divulgacoes" variant="h6">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </Link>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate id culpa quis in fugit nobis officiis expedita commodi dicta odit distinctio veniam ...
-            </Typography>
-            <br />
-            <Chip label="Direito Empresarial" />
-          </Grid>
+        {/* Filtro */}
+
+        <Grid container spacing={1} justifyContent="center">
+          {divulgacoes == "" && (
+            <>
+              <Typography style={{ fontWeight: 600 }}>
+                Nenhuma divulgação encontrada
+              </Typography>
+            </>
+          )}
         </Grid>
 
-        <br />
-        <Divider />
-        <br />
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm container spacing={1}>
+            {divulgacoes.map((divulgacao) => (
+              <Grid key={divulgacao.id_divulgacao} item xs={12}>
+                <Chip 
+                  variant="outlined" 
+                  color="primary" 
+                  size="small" 
+                  icon={<EventAvailableIcon />} 
+                  label={ formatDate(divulgacao.dt_cadastro) } 
+                />
+                <br />
+                <br />
+                <Link href="/cliente/minhas-divulgacoes" variant="h6">
+                  { divulgacao.titulo }
+                </Link>
+                <Typography>
+                  { divulgacao.descricao }
+                </Typography>
+                <br />
+                <Chip label={ divulgacao.areaAtuacao.titulo } />
+                <br />
+                <br />
+                <Divider />
+                <br />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
