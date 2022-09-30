4144880ada86400fdd13a56ab4f9f8cb3cffb1ce
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-09-29 21:33:49 -0300

    feat: tratamento para filtros de data no servico de buscar divulgacoes do cliente

--- src/services/client.service.js
+++ src/services/client.service.js
@@ -11,8 +11,14 @@ class ClientService {
     return api.get(url);
   }
 
-  getAllDivulgations(client_id) {
-    return api.get(`clients/divulgations/${client_id}`);
+  getAllDivulgations(client_id, data_inicial = null, data_final = null) {
+    let url = `clients/divulgations/${client_id}`;
+
+    if (data_inicial != null && data_final != null) {
+      url = `${url}/?data_inicial=${data_inicial}&data_final=${data_final}`
+    }
+
+    return api.get(url);
   }
 }
 
