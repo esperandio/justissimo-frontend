d85c36c136a4c619e6580a1cac8db5e20867e8b1
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-12-03 20:21:12 -0300

    refactor: removida a logica de realizar agendamento da tela de informacoes do advogado e centralizado no componente RealizarAgendamento

--- src/pages/InformacoesAdvogado/index.js
+++ src/pages/InformacoesAdvogado/index.js
@@ -3,41 +3,26 @@ import { useParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { 
   Container, 
-  Divider,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem
+  Divider
 } from "@material-ui/core";
 import { 
   Button,
   Rating,
-  TextField,
-  Radio,
-  RadioGroup,
-  FormControlLabel,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
   Stack,
   Backdrop,
   CircularProgress,
   Avatar
 } from "@mui/material";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import StarHalfIcon from "@mui/icons-material/StarHalf";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import { TitlePage }  from "../../components/Utils/title";
 import api from "../../services/api";
-import { ptBR } from "date-fns/locale";
 import { useHistory } from "react-router-dom";
 import Header from "../Main/Header";
 import Footer from "../Main/Footer";
 import ButtonWithTooltip from "../../components/ButtonWithTooltip";
+import RealizarAgendamento from "../../components/RealizarAgendamento";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -61,13 +46,6 @@ export default function InformacoesAdvogado() {
   const [backdropOpen, setBackdropOpen] = useState(true);
   const [advogado, setAdvogado] = useState({});
   const [advogadoAreas, setAdvogadoAreas] = useState([]);
-  const [dataAgendamento, setDataAgendamento] = useState(new Date());
-  const [id_area_atuacao, setAreaAtuacao] = useState("");
-  const [horarioAgendamento, setHorarioAgendamento] = useState("");
-  const [observacao, setObservacao] = useState("");
-
-  const [horarios, setHorarios] = useState([]);
-  const [exibirHorariosDisponiveis, setExibirHorariosDisponiveis] = useState(false);
   const [avaliacoes, setAvaliacoes] = useState([]);
 
   const[autenticado, setAutenticado] = useState(false);
@@ -97,95 +75,6 @@ export default function InformacoesAdvogado() {
 
   function handleClickFecharModalAgendamento() {
     setOpen(false);
-    setDataAgendamento(new Date());
-    setAreaAtuacao("");
-    setHorarioAgendamento("");
-    setObservacao("");
-
-    setExibirHorariosDisponiveis(false);
-  }
-
-  async function handleClickConfirmarAgendamento() {
-    try {
-      const fk_advogado = advogado?.id_advogado;
-      const fk_cliente = parseInt(sessionStorage.getItem("id_cliente"));
-      const fk_advogado_area = id_area_atuacao;
-      const data_agendamento = `${dataAgendamento.getUTCFullYear()}` 
-        + "-"
-        + `${dataAgendamento.getUTCMonth() + 1}`.padStart(2, 0)
-        + "-"
-        + `${dataAgendamento.getDate()}`.padStart(2, 0);
-      const horario = horarioAgendamento; 
-
-      const dados = {
-        fk_advogado,
-        fk_cliente,
-        fk_advogado_area,
-        data_agendamento,
-        horario,
-        observacao
-      }
-
-      await api.post("clients/scheduling", dados);
-
-      setOpen(false);
-
-      alert("Agendamento confirmado!!!");
-
-      setDataAgendamento(new Date());
-      setAreaAtuacao("");
-      setHorarioAgendamento("");
-      setObservacao("");
-
-      setExibirHorariosDisponiveis(false);
-    } catch (error) {
-      const mensagem_retorno_api = error?.response?.data?.message;
-
-      if (mensagem_retorno_api == null) {
-        alert("🤨 Algo deu errado! Tente novamente mais tarde");
-        return ;
-      }
-
-      alert(mensagem_retorno_api);
-    }
-  }
-
-  async function handleClickBuscarHorarios() {
-    if (id_area_atuacao === "") {
-      return;
-    }
-
-    setExibirHorariosDisponiveis(false);
-
-    const dataAgendamentoFormatada = `${dataAgendamento.getUTCFullYear()}` 
-      + "-"
-      + `${dataAgendamento.getUTCMonth() + 1}`.padStart(2, 0)
-      + "-"
-      + `${dataAgendamento.getDate()}`.padStart(2, 0);
-
-    try {
-      const horarios = await api.get(`hour-schedulings/${advogado?.id_advogado}?data_para_agendamento=${dataAgendamentoFormatada}`);
-
-      setHorarios(horarios.data?.horarios_disponiveis ?? []);
-
-      setExibirHorariosDisponiveis(true);
-    } catch (error) {
-      const mensagem_retorno_api = error?.response?.data?.message;
-
-      if (mensagem_retorno_api == null) {
-        alert("🤨 Algo deu errado! Tente novamente mais tarde");
-        return ;
-      }
-
-      alert(mensagem_retorno_api);
-    }
-  }
-
-  function handleChangeDataAgendamento(newValue) {
-    setDataAgendamento(newValue);
-
-    setHorarios([]);
-    setExibirHorariosDisponiveis(false);
   }
 
   function handleClickAvaliarAdvogado() {
@@ -314,113 +203,12 @@ export default function InformacoesAdvogado() {
           </div>
         )}
 
-        <Dialog open={open} onClose={ handleClickFecharModalAgendamento }>
-          <DialogTitle>Realizar agendamento</DialogTitle>
-
-          <DialogContent>
-            <DialogContentText>
-              1° Passo - Selecionar a data do agendamento e área de atuação
-            </DialogContentText>
-
-            <br />
-
-            <FormControl fullWidth className={classes.margin}>
-              <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
-                <DatePicker
-                  label="Data do agendamento"
-                  value={dataAgendamento}
-                  onChange={(newValue) => { handleChangeDataAgendamento(newValue) }}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </LocalizationProvider>
-            </FormControl>
-
-            <FormControl fullWidth variant="outlined" margin="normal" className={classes.margin}>
-              <InputLabel id="Area">Área de atuação</InputLabel>
-              <Select
-                required
-                labelId="Área de atuação"
-                id="AreaSelect"
-                multiline
-                variant="outlined"
-                value={id_area_atuacao}
-                onChange={e => setAreaAtuacao(e.target.value)}
-                label="Tipo de Usuario"
-              >
-                {advogadoAreas.map((area)=>{
-                  return <MenuItem key={area.areaAtuacao.id_area_atuacao} value={area.areaAtuacao.id_area_atuacao}>{area.areaAtuacao.titulo}</MenuItem>
-                })}
-              </Select>
-            </FormControl>
-
-            <br />
-            <br />
-
-            <Button 
-              variant="contained"
-              color="primary"
-              onClick={ handleClickBuscarHorarios }
-            >
-              Buscar horários
-            </Button>
-
-            <br/>
-            <br/>
-
-            {exibirHorariosDisponiveis === true && (
-              <>
-                <DialogContentText>
-                  2° Passo - Selecionar o horário do agendamento
-                </DialogContentText>
-
-                <FormControl>
-                  <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-                  >
-                    {horarios.map((x) => {
-                      return <FormControlLabel 
-                        key={x} 
-                        value={x} 
-                        checked={x === horarioAgendamento}
-                        control={<Radio />} label={x} onChange={() => setHorarioAgendamento(x)} 
-                      />
-                    })}
-                  </RadioGroup>
-                </FormControl>
-
-                <br />
-                <br />
-
-                <DialogContentText>
-                  3° Passo - Adicione uma observação
-                </DialogContentText>
-
-                <FormControl fullWidth className={classes.margin}>
-                  <TextField
-                    required
-                    id="Nome"
-                    label="Observação"
-                    placeholder="Observação"
-                    multiline
-                    minRows={5}
-                    variant="outlined"
-                    value={observacao}
-                    inputProps={{ maxLength: 200 }}
-                    onChange={e => setObservacao(e.target.value)}
-                    margin="normal"
-                  />
-                </FormControl>
-              </>
-            )}
-          </DialogContent>
-
-          <DialogActions>
-            <Button onClick={handleClickFecharModalAgendamento}>Cancelar</Button>
-            <Button onClick={handleClickConfirmarAgendamento} disabled={horarioAgendamento === "" || observacao === ""}>Confirmar</Button>
-          </DialogActions>
-        </Dialog>
+        <RealizarAgendamento
+          open={open}
+          advogado={advogado}
+          areas={advogadoAreas}
+          onClose={ handleClickFecharModalAgendamento }
+        ></RealizarAgendamento>
       </Container>
       <Footer />
     </>
