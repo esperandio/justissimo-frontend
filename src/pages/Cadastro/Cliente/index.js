177164bf0cf13a10835ebe5c929f96543036dbc1
Matheus Felipe Esperandio <mat.esperandio@gmail.com>
2022-12-03 15:24:39 -0300

    feat: ajustado para que a tela de login seja divida em steps (dados pessoais e dados de acesso)

--- src/pages/Cadastro/Cliente/index.js
+++ src/pages/Cadastro/Cliente/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import api from "../../../services/api";
-import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@mui/material/TextField";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
@@ -8,6 +7,10 @@ import FormControl from "@material-ui/core/FormControl";
 import Grid from "@material-ui/core/Grid";
 import SaveIcon from "@material-ui/icons/Save";
 import InputLabel from "@material-ui/core/InputLabel";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Box from "@mui/material/Box";
 import Header from "../../Main/Header";
 import Footer from "../../Main/Footer";
 import { Redirect } from "react-router-dom";
@@ -22,26 +25,7 @@ import InputCnpjMask from "../../../components/Utils/mask/inputCnpjMask";
 import InputCpfMask from "../../../components/Utils/mask/inputCpfMask";
 import TextFieldPassword from "../../../components/TextFieldPassword"
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  user: {
-    width: "20vh",
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  teste: {
-    minHeight: "56px"
-  }
-}));
-
 export default function CadastroUsuario() {
-
-  const classes = useStyles();
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
@@ -54,9 +38,12 @@ export default function CadastroUsuario() {
   const [estado, setEstado] = useState("");
   const [tipoPessoa, setTipoPessoa] = useState("Física");
   const [redirect, setState] = useState(false);
+  const [activeStep, setActiveStep] = useState(0);
 
   const [estados] = useState(["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
     "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]);
+  
+  const steps = ["Dados pessoais", "Dados de acesso"];
 
   const [tiposPessoa] = useState(["Física", "Jurídica"]);
 
@@ -109,6 +96,19 @@ export default function CadastroUsuario() {
 
   }
 
+  function handleClickSubmit(e) {
+    e.preventDefault();
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+
+    if (activeStep >= (steps.length - 1)) {
+      handleCadastro(e);
+    }
+  }
+
+  function handleClickVoltar() {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  }
+
   // Se o 'login' for aceito, redireciona para a tela de home
   if (redirect) {
     return <Redirect to='../login' />;
@@ -120,145 +120,187 @@ export default function CadastroUsuario() {
       <Container maxWidth="lg">
         <TitlePage internal="Cadastro de cliente" />
 
-        <form onSubmit={handleCadastro}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label) => {
+            const stepProps = {};
+            const labelProps = {};
+
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+
+        <form onSubmit={ handleClickSubmit }>
           <Grid container spacing={2}>
             <Grid item xs={12} sm container spacing={1}>
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <TextField
-                    required
-                    id="Nome"
-                    label="Nome"
-                    placeholder="Digite o nome completo"
-                    multiline
-                    variant="outlined"
-                    value={nome}
-                    onChange={e => setNome(e.target.value)}
-                    margin="normal"
-                  />
-                </FormControl>
-              </Grid>
+              {/* 1ª etapa */}
+              {activeStep === 0 && (
+                <>
+                  <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                      <TextField
+                        required
+                        id="Nome"
+                        label="Nome"
+                        placeholder="Digite o nome completo"
+                        multiline
+                        variant="outlined"
+                        value={nome}
+                        onChange={e => setNome(e.target.value)}
+                        margin="normal"
+                      />
+                    </FormControl>
+                  </Grid>
 
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
-                    <DatePicker
-                      label="Data de nascimento"
-                      value={dt_nascimento}
-                      onChange={newValue => setNascimento(newValue)}
-                      renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" />}
-                    />
-                  </LocalizationProvider>
-                </FormControl>
-              </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+                        <DatePicker
+                          label="Data de nascimento"
+                          value={dt_nascimento}
+                          onChange={newValue => setNascimento(newValue)}
+                          renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" />}
+                        />
+                      </LocalizationProvider>
+                    </FormControl>
+                  </Grid>
 
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <Autocomplete
-                    options={ tiposPessoa }
-                    isOptionEqualToValue={(option, value) => option.value === value.value}
-                    onChange={ (_, v) => { setTipoPessoa(v); setCPF(""); setCNPJ("") } }
-                    clearIcon={ false }
-                    value={ tipoPessoa }
-                    renderInput={(params) => <TextField {...params}  variant="outlined" margin="normal" label="Tipo de pessoa" />}
-                  />
-                </FormControl>
-              </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                      <Autocomplete
+                        options={ tiposPessoa }
+                        isOptionEqualToValue={(option, value) => option.value === value.value}
+                        onChange={ (_, v) => { setTipoPessoa(v); setCPF(""); setCNPJ("") } }
+                        clearIcon={ false }
+                        value={ tipoPessoa }
+                        renderInput={(params) => <TextField {...params}  variant="outlined" margin="normal" label="Tipo de pessoa" />}
+                      />
+                    </FormControl>
+                  </Grid>
 
-              <Grid item xs={12} sm={6}>
-                <FormControl fullWidth>
-                  <InputLabel id="Tipo"></InputLabel>
-                  {tipoPessoa === "Física"
-                    ? <InputCpfMask required onChange={e => setCPF(e.target.value)} />
-                    : <InputCnpjMask required onChange={e => setCNPJ(e.target.value)} />
-                  }
-                </FormControl>
-              </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <FormControl fullWidth>
+                      <InputLabel id="Tipo"></InputLabel>
+                      {tipoPessoa === "Física"
+                        ? <InputCpfMask required onChange={e => setCPF(e.target.value)} />
+                        : <InputCnpjMask required onChange={e => setCNPJ(e.target.value)} />
+                      }
+                    </FormControl>
+                  </Grid>
 
-              <Grid item xs={12} sm={4}>
-                <FormControl fullWidth>
-                  <InputLabel id="Tipo"></InputLabel>
-                  <InputCepMask  
-                    value={cep}
-                    onChange={e => setCEP(e.target.value)}
-                    required
-                  />
-                </FormControl>
-              </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <FormControl fullWidth>
+                      <InputLabel id="Tipo"></InputLabel>
+                      <InputCepMask  
+                        value={cep}
+                        onChange={e => setCEP(e.target.value)}
+                        required
+                      />
+                    </FormControl>
+                  </Grid>
 
-              <Grid item xs={12} sm={4}>
-                <FormControl fullWidth>
-                  <Autocomplete
-                    options={estados}
-                    isOptionEqualToValue={(option, value) => option.value === value.value}
-                    onChange={ (_, v) => { setEstado(v); } }
-                    renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" label="Estado" />}
-                  />
-                </FormControl>
-              </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <FormControl fullWidth>
+                      <Autocomplete
+                        options={estados}
+                        isOptionEqualToValue={(option, value) => option.value === value.value}
+                        onChange={ (_, v) => { setEstado(v); } }
+                        renderInput={(params) => <TextField {...params} required variant="outlined" margin="normal" label="Estado" />}
+                      />
+                    </FormControl>
+                  </Grid>
 
-              <Grid item xs={12} sm={4}>
-                <FormControl fullWidth>
-                  <TextField
-                    required
-                    id="Cidade"
-                    label="Cidade"
-                    placeholder="Nome da Cidade"
-                    multiline
-                    variant="outlined"
-                    value={cidade}
-                    onChange={e => setCidade(e.target.value)}
-                    margin="normal"
-                  />
-                </FormControl>
-              </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <FormControl fullWidth>
+                      <TextField
+                        required
+                        id="Cidade"
+                        label="Cidade"
+                        placeholder="Nome da Cidade"
+                        multiline
+                        variant="outlined"
+                        value={cidade}
+                        onChange={e => setCidade(e.target.value)}
+                        margin="normal"
+                      />
+                    </FormControl>
+                  </Grid>
+                </>
+              )}
 
-              <Grid item xs={12} sm={12}>
-                <FormControl fullWidth>
-                  <TextField
-                    required
-                    id="email"
-                    label="E-mail"
-                    placeholder="meuemail@email.com"
-                    multiline
-                    variant="outlined"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    margin="normal"
-                  />
-                </FormControl>
-              </Grid>
-                            
-              <Grid item xs={12} sm={6}>
-                <TextFieldPassword onChange={e => setSenha(e.target.value)}></TextFieldPassword>
-              </Grid>
+              {/* 2ª etapa */}
+              {activeStep >= 1 && (
+                <>
+                  <Grid item xs={12} sm={12}>
+                    <FormControl fullWidth>
+                      <TextField
+                        required
+                        id="email"
+                        label="E-mail"
+                        placeholder="meuemail@email.com"
+                        multiline
+                        variant="outlined"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        margin="normal"
+                      />
+                    </FormControl>
+                  </Grid>
+                                
+                  <Grid item xs={12} sm={6}>
+                    <TextFieldPassword onChange={e => setSenha(e.target.value)}></TextFieldPassword>
+                  </Grid>
 
-              <Grid item xs={12} sm={6}>
-                <TextFieldPassword onChange={e => setSenhaConfirmacao(e.target.value)}></TextFieldPassword>
-              </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextFieldPassword onChange={e => setSenhaConfirmacao(e.target.value)}></TextFieldPassword>
+                  </Grid>
 
-              <Grid item xs={12} sm={12}>
-                <span>Mínimo 8 caracteres</span> <br/>
-                <span>Caracteres maiúsculos</span> <br/>
-                <span>Caracteres mínusculos</span> <br/>
-                <span>Símbolos ou números</span> <br/>
-              </Grid>
-
-              <Grid item xs={12} sm={12}>
-                <FormControl>
-                  <Button className={classes.submit}
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    startIcon={<SaveIcon />}
-                  >
-                    Cadastrar
-                  </Button>
-                </FormControl>
-              </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <span>Mínimo 8 caracteres</span> <br/>
+                    <span>Caracteres maiúsculos</span> <br/>
+                    <span>Caracteres mínusculos</span> <br/>
+                    <span>Símbolos ou números</span> <br/>
+                  </Grid>
+                </>
+              )}
             </Grid>
           </Grid>
 
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+            <Button
+              variant="contained"
+              disabled={activeStep === 0}
+              onClick={ handleClickVoltar }
+              sx={{ mr: 1 }}
+            >
+              Voltar
+            </Button>
+
+            { activeStep >= (steps.length - 1) 
+              ? <>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  startIcon={<SaveIcon />}
+                >
+                  Cadastrar
+                </Button>
+              </>
+              : <>
+                <Button 
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Próximo
+                </Button>
+              </>
+            }
+          </Box>
         </form>
       </Container>
       <Footer />
