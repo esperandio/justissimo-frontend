19245968c05c39f946609df5b101c65ca7f2f316
David de Jesus Oliveira <67175850+David-Jesus@users.noreply.github.com>
2022-12-04 00:11:00 -0300

    feat: inclusao de menu para admin

--- src/pages/Main/Header.js
+++ src/pages/Main/Header.js
@@ -34,12 +34,14 @@ const Header = () => {
   const [settings] = useState(["Editar perfil", "Logout"]);
 
   useEffect(() => {
-    let pages = ["Home", "Pesquisar Advogado", "Cadastrar Cliente", "Cadastrar Advogado"];
+    let pages = ["Home", "Pesquisar Advogado", "Cadastrar Cliente", "Cadastrar Advogado", "Aprovar Advogados"];
 
     if (sessionStorage.getItem("tipo_usuario") === "Advogado") {
       pages = ["Home", "Meus agendamentos", "Buscar Divulgações"];
     } else if (sessionStorage.getItem("tipo_usuario") === "Cliente") {
       pages = ["Home", "Meus agendamentos", "Minhas Divulgações", "Pesquisar Advogado", "Cadastrar Divulgação"];
+    } else if (sessionStorage.getItem("tipo_usuario") === "Administrador") {
+      pages = ["Home", "Pesquisar Advogado", "Aprovar Advogados"];
     }
 
     setPages(pages);
@@ -81,6 +83,8 @@ const Header = () => {
       return "/cliente/cad";
     case "Cadastrar Advogado":
       return "/advogado/cad";
+    case "Aprovar Advogados":
+      return "/admin/aprovar-advogados";
     default:
       return "/home";
     }
@@ -236,10 +240,12 @@ const Header = () => {
                     onClose={handleCloseUserMenu}
                   >
                     {settings.map((setting) => (
-                      <MenuItem key={setting} onClick={() => handleClickSettingsMenuItem(setting)}>
-                        <Typography textAlign="center">{setting}</Typography>
-                      </MenuItem>
-                    ))}
+                      //caso for administrador não deve aparecer a opção de editar perfil 
+                      (setting === "Editar perfil" && sessionStorage.getItem("tipo_usuario") === "Administrador") ? null : (
+                        <MenuItem key={setting} onClick={() => handleClickSettingsMenuItem(setting)}>
+                          <Typography textAlign="center">{setting}</Typography>
+                        </MenuItem>
+                      )))}
                   </Menu>
                 </>
               }
